d3a8058f3d2d4bc701d2c4d497bcd8220013812e
good996 <sbatanye@gmail.com>
2020-03-26 17:59:11 +0100

    set config app_name

--- resources/views/layouts/partial/_footer.blade.php
+++ resources/views/layouts/partial/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="text-center">
-	<p>&copy; {{date('Y')}} &middot; laracarte by <a href="https://twitter.com/etsmo">@etsmo</a>
+	<p>&copy; {{date('Y')}} &middot; {{config('app.name')}} by <a href="https://twitter.com/etsmo">@etsmo</a>
 	- A <a href="https://laramap.com">Laramap</a> clone app.</p>
 	<p><strong>This app as been build for learning purpose.</strong></p>
 </footer>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,8 +4,8 @@
 
 <div class="container">
 
-	<h2>Whhat is Laracarte ?</h2>
-	<p>Laracarte is a clone app of <a href="https://laramp.com" target=".bllank">laramap.com</a> .</p>
+	<h2>Whhat is {{config('app.name')}} ?</h2>
+	<p>{{config('app.name')}} is a clone app of <a href="https://laramp.com" target=".bllank">laramap.com</a> .</p>
 	<div class="row">
 		<div class="col-md-6">
 			<p class="alert alert-warning">
@@ -18,11 +18,11 @@
 
 	<hr>
 	 <h2>What is Laramap ?</h2>
-	 <p>Laramap is a website by which Laracarte was inspired :).</p>
+	 <p>Laramap is a website by which {{config('app.name')}} was inspired :).</p>
 	 <p>More info <a href="https://laramap.com/about"> here</a>.</p>
 
 	 <hr>
-	 <h2>Which tools and services are used in laracarte ?</h2>
+	 <h2>Which tools and services are used in {{config('app.name')}} ?</h2>
 	 <p>Basically it is build on laravel &amp; Bootstrap. but thre is a bunch of services used for email and other sections.</p>
 	 <ul>
 	 	<li>Laravel</li>
